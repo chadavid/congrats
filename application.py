adcc78c45539ded76b98689cf428554ed960f7bc
David Chapman <chadavid@amazon.com>
2014-05-27 17:42:17 -0400

    update

--- application.py
+++ application.py
@@ -88,7 +88,7 @@ response = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Eongratulations</h1>
+    <h1>fongratulations</h1>
     <p>Your <strong>second</strong> AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
